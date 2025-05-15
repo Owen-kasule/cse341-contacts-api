f9f35e9848b7c574521fffe9246afdda43dda8be
Owen-kasule <owenatug@gmail.com>
2025-05-15 11:28:33 +0300

    Refactor: remove user-related routes and database connection code

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import * as usersController from '../controllers/users.js';
-
-const router = express.Router();
-
-router.get('/', usersController.getAll);
-router.get('/:id', usersController.getSingle);
-
-export default router;
